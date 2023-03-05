66541f3b5920ab954fd391109e9448be2e517c77
nigel2000 <d.jones560@ntlworld.com>
2023-03-05 09:17:54 +0000

    Changing Folder

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+let shopItemsData = [
+    {
+        id: "hfhkhfeh",
+        name: "Karcher Washer",
+        price: "198.78",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "./images/10938.jpg",
+    },
+    {
+        id: "dldldvldk",
+        name: "Cordless Drill",
+        price: "79.98",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "./images/16201.jpg",
+    },
+    {
+        id: "faflsij",
+        name: "Renovation Screed",
+        price: "20.99",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "./images/21200.jpg",
+    },
+    {
+        id: "dshwhijl",
+        name: "Laser Tape measure",
+        price: "12.99",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "./images/95034.jpg",
+    },
+    
+
+]
---
+++ src/cart.js
@@ -0,0 +1,29 @@
+let label = document.getElementById("label")
+let shoppingCart = document.getElementById("shopping-cart")
+
+let basket = JSON.parse(localStorage.getItem("data")) || []
+
+let calculation = ()=> {
+    let cartIcon = document.getElementById("cartAmount")
+
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y)=> x+y, 0)
+}
+
+calculation()
+
+let generateCartItems = () => {
+    if (basket.length != 0) {
+        
+    }
+    else{
+        shoppingCart.innerHTML = ``
+        label.innerHTML = `
+        <h2>Cart is Empty<h2>
+        <a href="index.html">
+        <button class="HomeBTN">Back to Home</button>
+        </a>
+        `
+    }
+}
+
+generateCartItems()
