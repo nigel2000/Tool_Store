b216fcf5cd985febe31d3534da39e550f838d79e
nigel2000 <d.jones560@ntlworld.com>
2023-03-06 19:19:24 +0000

    cart items

--- src/cart.js
+++ src/cart.js
@@ -4,7 +4,7 @@ let shoppingCart = document.getElementById("shopping-cart")
 let basket = JSON.parse(localStorage.getItem("data")) || []
 
 let calculation = ()=> {
-    let cartIcon = document.getElementById("cartAmount")
+   let cartIcon = document.getElementById("cartAmount")
 
     cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y)=> x+y, 0)
 }
@@ -12,13 +12,35 @@ let calculation = ()=> {
 calculation()
 
 let generateCartItems = () => {
-    if (basket.length != 0) {
-        
+    if (basket.length !== 0) {
+        return (shoppingCart.innerHTML = basket.map((x) => {
+            let {id, item} = x
+            // x is in the basket y is in the data.js
+            let search = shopItemsData.find((y) => y.id === id) || []
+            return `
+            <div class="cart-item">
+            <img width="100" src=${search.img} alt=" />
+            <div class="details">
+                <div class="title-price-x">
+                   <h4>
+                    <p>${search.name}</p>
+                    <p>£${search.price}</p>
+                   </h4> 
+                   <i class="bi bi-x-lg"></i>
+                </div>
+
+                <div class="cart-buttons"></div>
+
+                <h3></h3>
+            </div>
+            </div>
+            `
+        }).join(""))
     }
     else{
         shoppingCart.innerHTML = ``
         label.innerHTML = `
-        <h2>Cart is Empty<h2>
+        <h2>Cart is Empty</h2>
         <a href="index.html">
         <button class="HomeBTN">Back to Home</button>
         </a>
